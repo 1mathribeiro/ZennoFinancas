72181944500c292cec8225c741e46e8db19af68f
1mathribeiro <mathribeiro777@gmail.com>
2025-08-27 19:59:21 -0300

    Novas Telas

---
+++ app/src/main/java/com/example/zenno/Verificacao.java
@@ -0,0 +1,14 @@
+package com.example.zenno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Verificacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verificacao);
+    }
+}
